44861c4c0efc9d98e4fbe18d78b5b0da8d75ab49
Nacho <joseignaciorm13@gmail.com>
2020-08-10 16:39:49 +0200

    Created CPT "note"

--- university-post-types.php
+++ university-post-types.php
@@ -83,6 +83,26 @@ if (!function_exists('university_post_types')) {
             ),
             'menu_icon' => 'dashicons-welcome-learn-more'
         ) );
+
+
+     /**
+         * Note Post Type
+         */
+        register_post_type( 'note', array(
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor'),
+            'public' => false,
+            'show_ui' => true,
+            'labels' => array(
+                'name' => 'Notes',
+                'add_new_item' => 'Add New Note',
+                'edit_item' => 'Edit Note',
+                'all_items' => 'All Notes',
+                'singular_name' => 'Note'
+            ),
+            'menu_icon' => 'dashicons-welcome-write-blog'
+        ) );
+    
     }
 }
 
