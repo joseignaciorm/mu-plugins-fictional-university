59c4c59a5ab5073f828642dc85a4a766dfc875ff
Nacho <joseignaciorm13@gmail.com>
2020-08-11 09:46:36 +0200

    Added capability in CPT Note

--- university-post-types.php
+++ university-post-types.php
@@ -89,6 +89,8 @@ if (!function_exists('university_post_types')) {
          * Note Post Type
          */
         register_post_type( 'note', array(
+            'capability_type' => 'note',
+            'map_meta_cap' => true,
             'show_in_rest' => true,
             'supports' => array('title', 'editor'),
             'public' => false,
